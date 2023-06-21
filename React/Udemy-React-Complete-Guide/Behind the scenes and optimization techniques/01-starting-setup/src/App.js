9ad4d103ee7bed3e1ca90522df13b89f7ef2e972
Adrian Albert Ciubucciu <adrian-albert.a.ciubucciu_c@sandvik.com>
2023-06-21 22:44:35 +0300

    practicing React.memo() and write notes

--- React/Udemy-React-Complete-Guide/Behind the scenes and optimization techniques/01-starting-setup/src/App.js
+++ React/Udemy-React-Complete-Guide/Behind the scenes and optimization techniques/01-starting-setup/src/App.js
@@ -20,6 +20,8 @@ function App() {
     <div className="app">
       <h1>Hi there!</h1>
       {/* {showP && <p>This is the p</p>} */}
+      {/* pass false value to the show prop and see that the DemoOutput will not re-render with the memo method, but 
+      if the state showP is passed trough prop, it will re-render because of the change. */}
       <DemoOutput show={false} />
       <Button onClick={toggleP}>Toggle P</Button>
     </div>
--- React/Udemy-React-Complete-Guide/Behind the scenes and optimization techniques/01-starting-setup/src/components/Demo/DemoOutput.js
+++ React/Udemy-React-Complete-Guide/Behind the scenes and optimization techniques/01-starting-setup/src/components/Demo/DemoOutput.js
@@ -5,4 +5,12 @@ const DemoOutput = (props) => {
   return <MyParagraph>{props.show ? "This is the paragraph" : ""}</MyParagraph>;
 };
 
-export default DemoOutput;
+/* This is for functional components. For class based components it will not work. It allows us to optimize functional 
+components. The method memo tells React for the component got as an argument, it should look at the props the component
+gets and check the new value for all those props and compare with the previous value those props got. And only if a prop
+changed the component should be re-eecuted and re-evaluated. Also, if the parent component changed but the prop values
+for the current component  didn't change, the component execution will be skipped
+But this optimization comes with a cost : the React needs to store previous values and also make the comparison.
+
+*/
+export default React.memo(DemoOutput);
