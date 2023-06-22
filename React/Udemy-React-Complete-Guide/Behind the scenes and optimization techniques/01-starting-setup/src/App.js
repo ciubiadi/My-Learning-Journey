3ffee0b3b85dc49eb3557ce5c44700d1c0929050
Adrian Albert Ciubucciu <adrian-albert.a.ciubucciu_c@sandvik.com>
2023-06-22 06:57:40 +0300

    practicing useCallback

--- React/Udemy-React-Complete-Guide/Behind the scenes and optimization techniques/01-starting-setup/src/App.js
+++ React/Udemy-React-Complete-Guide/Behind the scenes and optimization techniques/01-starting-setup/src/App.js
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+/*
+useCallback allows us to store a function across component execution. It allows us to tell React that I want ot save 
+a function and this function should not be recreated with every component execution
+*/
+import React, { useState, useCallback } from "react";
 import Button from "./components/UI/Button/Button";
 import DemoOutput from "./components/Demo/DemoOutput";
 
@@ -6,12 +10,21 @@ import "./App.css";
 
 function App() {
   const [showP, setShowP] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log("app is running");
 
-  const toggleP = () => {
-    // setShowP(!showP);
-    setShowP((prevShowP) => !prevShowP);
+  //Basically if I know that this function will never change, I can use useCallback to store it
+  //useCallback allows me to save the function so I can reuse it.
+  const toggleP = useCallback(() => {
+    if (allowToggle) {
+      // setShowP(!showP);
+      setShowP((prevShowP) => !prevShowP);
+    }
+  }, [allowToggle]);
+
+  const alowToggleHandler = () => {
+    setAllowToggle(true);
   };
 
   return (
@@ -22,15 +35,11 @@ function App() {
       {/* {showP && <p>This is the p</p>} */}
       {/* pass false value to the show prop and see that the DemoOutput will not re-render with the memo method, but 
       if the state showP is passed trough prop, it will re-render because of the change. */}
-      <DemoOutput show={false} />
+      <DemoOutput show={showP} />
+      <Button onClick={alowToggleHandler}>Allow toggling</Button>
       <Button onClick={toggleP}>Toggle P</Button>
     </div>
   );
 }
 
 export default App;
-
-/*
-
-
-*/
