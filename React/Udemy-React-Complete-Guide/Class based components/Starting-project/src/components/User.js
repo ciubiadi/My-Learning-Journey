d0ff0b15a00028463a6fcab132b89932e9a19d2e
Adrian Albert Ciubucciu <adrian-albert.a.ciubucciu_c@sandvik.com>
2023-06-22 10:40:27 +0300

    refactor to class based components

---
+++ React/Udemy-React-Complete-Guide/Class based components/Starting-project/src/components/User.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+import classes from "./User.module.css";
+
+class User extends Component {
+  // constructor() {
+  //   super();
+  // }
+  render() {
+    // we can access props trough this because of the class Component we extend
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
+
+export default User;
---
+++ React/Udemy-React-Complete-Guide/Class based components/Starting-project/src/components/Users.js
@@ -0,0 +1,86 @@
+import { Component } from "react";
+// import { useState } from "react";
+import User from "./User";
+import classes from "./Users.module.css";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
+
+class Users extends Component {
+  // to define state in class based component I use constructor
+  // [showUsers, setShowUsers] = useState(true);
+  // This construcot is automatically called whenever the class is instantiated
+  constructor() {
+    super();
+    // In Class based components the state is ALWAYS stored in a object
+    // So the state is always an object and it also has to be a property named "state"
+    // In other words, I need to store togheter in a object all state pieces
+    this.state = {
+      showUsers: true,
+    };
+  }
+
+  toggleUsersHandler() {
+    // setShowUsers((curState) => !curState);
+    // this.state.showUsers = false    ---- this is NOT the correct way to modidfy the state
+
+    //The corect way to modify the state is the below one
+    // this.setState({
+    //   showUsers: false,
+    // });
+
+    this.setState((curState) => {
+      return {
+        showusers: !curState.showUsers,
+      };
+    });
+  }
+
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? "Hide" : "Show"} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+// const Users = () => {
+//   const [showUsers, setShowUsers] = useState(true);
+
+//   const toggleUsersHandler = () => {
+//     setShowUsers((curState) => !curState);
+//   };
+
+//   const usersList = (
+//     <ul>
+//       {DUMMY_USERS.map((user) => (
+//         <User key={user.id} name={user.name} />
+//       ))}
+//     </ul>
+//   );
+
+//   return (
+//     <div className={classes.users}>
+//       <button onClick={toggleUsersHandler}>
+//         {showUsers ? "Hide" : "Show"} Users
+//       </button>
+//       {showUsers && usersList}
+//     </div>
+//   );
+// };
+
+export default Users;
