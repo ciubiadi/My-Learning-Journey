76832a8b6544343f11a24ae168c8d8e405f1bc5e
Adrian Albert Ciubucciu <adrian-albert.a.ciubucciu_c@sandvik.com>
2023-07-16 20:26:17 +0300

    adding Loading state and style the text

--- React/Udemy-React-Complete-Guide/food-order-app/src/components/Meals/Meals.js
+++ React/Udemy-React-Complete-Guide/food-order-app/src/components/Meals/Meals.js
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Meals.module.css";
 import { DUMMY_MEALS } from "../../assets/utils/dummy-meals";
 import Card from "../UI/Card/Card";
 import MealItem from "./MealItem/MealItem";
 
 const Meals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem key={meal.id} meal={meal} />
-  ));
+  const [meals, setMeals] = useState(DUMMY_MEALS);
+  // I'm setting this state to true from the beginning because I know everytime the meals will be loadewd so the app will load them
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://food-order-app-5060a-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+      const responseData = await response.json();
+      console.log(responseData);
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+    fetchMeals();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={styles.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => <MealItem key={meal.id} meal={meal} />);
 
   return (
     <section className={styles.meals}>
