55f9d773d5e81dd50b381982ad9b1442e0cb6876
ciubuadi <ciubucciu.adrianalbert@gmail.com>
2023-07-25 23:47:06 +0300

    add 1 more type

--- React/Udemy-React-Complete-Guide/redux-basics/demo-redux-basics.js
+++ React/Udemy-React-Complete-Guide/redux-basics/demo-redux-basics.js
@@ -1,9 +1,17 @@
 const redux = require('redux');
 
-const storeReducer = (state = {counter:0}, action) => {
-    return {
-        counter: state.counter + 1
+const storeReducer = (state = { counter:0 }, action) => {
+    if(action.type === 'increment'){
+        return {
+            counter: state.counter + 1
+        }
     }
+    if(action.type === 'decrement'){
+        return {
+            counter: state.counter -1
+        }
+    }
+    return state;
 };
 
 const store = redux.createStore(storeReducer);
@@ -13,6 +21,9 @@ const stateSubscriber = () => {
     console.log(lastState);
 };
 
+//this will output the first state : {counter: 1}
 store.subscribe(stateSubscriber);
 
-store.dispatch({type: 'increment'})
+store.dispatch({type: 'increment'});
+// because we change the state decreasing the counter, it will retrigger the subscruption so it will ooutput {counter: 0}
+store.dispatch({type: 'decrement'});
