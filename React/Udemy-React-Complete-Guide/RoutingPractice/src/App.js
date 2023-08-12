72dd19f16b78902d4d8c4e75099750293f03c40a
ciubuadi <ciubucciu.adrianalbert@gmail.com>
2023-08-12 10:33:27 +0300

    another way to use routes - createRoutesFromElements

--- React/Udemy-React-Complete-Guide/RoutingPractice/src/App.js
+++ React/Udemy-React-Complete-Guide/RoutingPractice/src/App.js
@@ -1,11 +1,22 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 import HomePage from './pages/Home.js';
 import ProductsPage from './pages/Products.js';
 
-const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/products', element: <ProductsPage />}
-]);
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/products" element={<ProductsPage />} />
+  </Route>
+)
+
+const router = createBrowserRouter(routeDefinitions);
+
+// const router = createBrowserRouter([
+//   { path: '/', element: <HomePage /> },
+//   { path: '/products', element: <ProductsPage />}
+// ]); 
+
+
 
 function App() {
   return <RouterProvider router={router} />;
