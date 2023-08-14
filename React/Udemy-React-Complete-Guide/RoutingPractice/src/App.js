0c23310ccd114f5a458a444d628793fdb87d522f
ciubuadi <ciubucciu.adrianalbert@gmail.com>
2023-08-14 23:14:24 +0300

    useParams practice

--- React/Udemy-React-Complete-Guide/RoutingPractice/src/App.js
+++ React/Udemy-React-Complete-Guide/RoutingPractice/src/App.js
@@ -3,6 +3,7 @@ import HomePage from './pages/Home.js';
 import ProductsPage from './pages/Products.js';
 import RootLayout from './pages/Root.js';
 import ErrorPage from './pages/Error.js';
+import ProductDetailPage from './pages/ProductDetail.js';
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -20,7 +21,8 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [ 
       { path: '/', element: <HomePage /> },
-      { path: '/products', element: <ProductsPage />}
+      { path: '/products', element: <ProductsPage />},
+      { path: '/products/:productId', element: <ProductDetailPage />}
     ]
   }
 ]); 
