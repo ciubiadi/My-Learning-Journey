ae274a7e039dcf82961e8ed1ec3338eba14c1db4
ciubuadi <ciubucciu.adrianalbert@gmail.com>
2023-08-19 10:05:57 +0300

    planning the data submission + form css

--- React/Udemy-React-Complete-Guide/FullStackJS/frontend/src/pages/NewEvent.js
+++ React/Udemy-React-Complete-Guide/FullStackJS/frontend/src/pages/NewEvent.js
@@ -1,6 +1,15 @@
+import EventForm from "../components/EventForm";
+
 const NewEventPage = () => {
+    // aooriach #1
+    const submitHandler = (event) => {
+        event.preventDefault();
+        /* extract data from the form with the help of two-way-binding or refs and manually sen HTTP request here,
+         manage some loading and error state and in the end navigate from this page once the user finished. 
+         ( navigate away with useNavigate hook ) */
+    }
     return (
-        <h1>NewEventPage</h1>
+        <EventForm />
     );
 }
 
