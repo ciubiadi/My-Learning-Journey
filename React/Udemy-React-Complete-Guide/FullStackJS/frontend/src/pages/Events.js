98bb843ffdcc0ea43091aba493c759f459a02901
ciubuadi <ciubucciu.adrianalbert@gmail.com>
2023-08-19 12:34:57 +0300

    Deferring Data fetching with defer()

--- React/Udemy-React-Complete-Guide/FullStackJS/frontend/src/pages/Events.js
+++ React/Udemy-React-Complete-Guide/FullStackJS/frontend/src/pages/Events.js
@@ -1,6 +1,7 @@
 // import { useEffect, useState } from 'react';
-import { useLoaderData, json } from 'react-router-dom';
+import { useLoaderData, json, defer, Await } from 'react-router-dom';
 import EventsList from '../components/EventsList';
+import { Suspense } from 'react';
 
 function EventsPage() {
 //   const [isLoading, setIsLoading] = useState(false);
@@ -28,14 +29,26 @@ function EventsPage() {
     // const events = useLoaderData();
 
     // #2
-    const data = useLoaderData();
-    if(data.isError){
-        return <p>{data.message}</p>
-    }
-    const events = data.events;
+    // const data = useLoaderData();
 
-  return (
-    <>
+    const {events} = useLoaderData();
+
+    // if(data.isError){
+    //     return <p>{data.message}</p>
+    // }
+
+    // const events = data.events;
+
+    return (
+        <Suspense fallback={<p style={{textAlign: 'center'}}>Loading...</p>}>
+            <Await resolve={events}>
+                { (loadedEvents) => <EventsList events={loadedEvents} />}
+            </Await>
+        </Suspense>
+    )
+
+//   return (
+//     <>
     {/* Approach #1 */}
       {/* <div style={{ textAlign: 'center' }}>
         {isLoading && <p>Loading...</p>}
@@ -44,21 +57,44 @@ function EventsPage() {
       {/* {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />} */}
 
       {/* Appraoch #2 */}
-      <EventsList events={events} />
+    //   <EventsList events={events} />
 
       {/* Approach #3 */}
       {/* <EventsList /> */}
-    </>
-  );
+//     </>
+//   );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents () {
     const response = await fetch('http://localhost:8080/events');
 
     if (!response.ok) {
-      console.log('Error!')
+        throw json(
+            {message: 'Could not fetch events.'}, 
+            {
+                status: 500,
+            }
+        );
+    } else {
+        // return response;
+        const resData = await response.json();
+        return resData.events;
+    }
+}
+
+export function loader() {
+    return defer({
+        events: loadEvents()
+    })
+}
+
+// export async function loader() {
+//     const response = await fetch('http://localhost:8080/events');
+
+//     if (!response.ok) {
+//       console.log('Error!')
     //   return {
     //     isError: true,
     //     message: 'Could not fetch events. '
@@ -72,13 +108,13 @@ export async function loader() {
         // }); 
 
         // thorw response #2 approach
-        throw json(
-            {message: 'Could not fetch events.'}, 
-            {
-                status: 500,
-            }
-        );
-    } else {
+    //     throw json(
+    //         {message: 'Could not fetch events.'}, 
+    //         {
+    //             status: 500,
+    //         }
+    //     );
+    // } else {
     //   const resData = await response.json();
         //   console.log('resData');
         //   console.log(resData);
@@ -88,6 +124,6 @@ export async function loader() {
         // const res= new Response('any data', {status: 201});
         // return res; 
 
-        return response;
-    }
-}
+//         return response;
+//     }
+// }
